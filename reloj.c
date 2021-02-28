ead34ae54bd0ee50dfaa31ecfc7cd61bbe2e2da1
edSantiago <santiago7nubesoft@gmail.com>
2021-02-28 14:45:36 -0600

    feat: agregar botón decremento para configuración
    Se cambio al pin B7 la salida de segundos

--- reloj.c
+++ reloj.c
@@ -20,10 +20,10 @@ void main(){
    set_tris_a(0b000000);  //(0)salida segundo, (1)setHora, , (2)setMin
    set_tris_c(0b00000000); //salida de bits, hora
    set_tris_d(0b00000000); //salida de bits, minuto   
-   set_tris_e(0b011); //(0)config más de 1 seg. (1) incremento
+   set_tris_e(0b111); //(0)config más de 1 seg. (1) incremento (2) decremento
    output_a(0x00);
    output_d(0x00);
-   output_c(0x00);       
+   output_c(0x00); 
    
    rtc_init();
       
@@ -46,8 +46,9 @@ void main(){
 
 void config(){
    int set = 0;   //0 min, 1 hora, 2 dia, 3 mes, 4 anio
+   int cambio;
    int anio, mes, dia, semana, hora, minuto, segundo;   
-   int inConfig, inIncremento;
+   int inConfig, inIncremento, inDecremento;
    
    while(set<6){
       inConfig = INPUT(PIN_E0);      
@@ -59,65 +60,66 @@ void config(){
       rtc_get_date(dia, mes, anio, semana);
       rtc_get_time(hora, minuto, segundo);
       if(set == 0){
-         output_a(0b000010);
-         output_low(PIN_E2);
+         output_a(0b000001);         
          mostrarHoraMinuto(hora, minuto);
       }else if(set == 1){
-         output_a(0b000100);
-         output_low(PIN_E2);
+         output_a(0b000010);         
          mostrarHoraMinuto(hora, minuto);
       }else if(set == 2){
-         output_a(0b001000);
-         output_low(PIN_E2);
+         output_a(0b000100);         
           mostrarHoraMinuto(0, dia);
       }else if(set == 3){
-         output_a(0b010000);
-         output_low(PIN_E2);
+         output_a(0b001000);         
          mostrarHoraMinuto(0, mes);
       }else if(set == 4){
-         output_a(0b100000);
-         output_low(PIN_E2);         
+         output_a(0b010000);                  
          mostrarHoraMinuto(0, anio);
       }else if(set == 5){
-         output_a(0b111111);
-         output_high(PIN_E2);
+         output_a(0b100000);         
          mostrarHoraMinuto(0, semana);
       }
       
       
-      inIncremento = INPUT(PIN_E1);      
-      if(inIncremento == 1){
+      inIncremento = INPUT(PIN_E2);
+      inDecremento = INPUT(PIN_E1);
+      if((inIncremento == 1)||(inDecremento == 1)){
+         if(inIncremento == 1) cambio = 1;
+         else cambio = -1;
+      
          delay_ms(600);
          rtc_get_time(hora, minuto, segundo); 
          
          if(set == 0){                         // minuto
-            minuto++;
-            if(minuto > 59) minuto = 0;
+            if((inDecremento == 1) &&(minuto == 0)) minuto = 59;
+            else minuto += cambio;            
+            if(minuto > 59) minuto = 0;            
             rtc_set_datetime(dia, mes, anio, semana, hora, minuto);
          }else if(set == 1){                  // hora
-            hora++;
-            if(hora > 23) hora = 0;
+            if((inDecremento == 1) && (hora == 0)) hora = 23;
+            else hora += cambio;
+            if(hora > 23) hora = 0;            
             rtc_set_datetime(dia, mes, anio, semana, hora, minuto);
          } else if(set == 2){                // día
-            dia++;
+            dia += cambio;
             dia = verificaFecha(dia, mes, anio);
             rtc_set_datetime(dia, mes, anio, semana, hora, minuto);         
          }else if(set == 3){                 // mes
-            mes++;
-           if(mes>12) mes = 1;
+            mes += cambio;
+            if(mes > 12) mes = 1;
+            if(mes < 1) mes = 12;
             rtc_set_datetime(dia, mes, anio, semana, hora, minuto);
          }else if(set == 4){                 //año
-            anio++;
+            anio += cambio;
             rtc_set_datetime(dia, mes, anio, semana, hora, minuto);
          }else if(set == 5){                 //semana 7=domingo
-            semana++;
+            semana += cambio;
             if(semana > 7) semana = 1;
+            if(semana < 1) semana = 7;
             rtc_set_datetime(dia, mes, anio, semana, hora, minuto);
          }         
       }      
    }
-   output_a(0b000000);
-   output_low(PIN_E2);
+   output_a(0b000000);   
 }
 
 void mostrarHoraMinuto(hora, minuto){
@@ -132,9 +134,9 @@ void mostrarHoraMinuto(hora, minuto){
 
 void mostrarSegundo(){
    if(OUT_SEGUNDO){
-      output_high(PIN_A0);
+      output_high(PIN_B7);
    }else{      
-      output_low(PIN_A0);
+      output_low(PIN_B7);
    }
    OUT_SEGUNDO = !OUT_SEGUNDO;
 }
@@ -154,8 +156,8 @@ int verificaFecha(int dia, int mes, int anio){
    
    if(dia > limiteDias){
       dia2 = dia - limiteDias;
-   }else dia2 = dia;
+   }else if(dia < 1) dia2 = limiteDias;
+   else dia2 = dia;
    
    return dia2;
 }
-
