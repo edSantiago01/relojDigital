ba4a1ab50d06d242ad208ac0c45d77a65cb11a67
edSantiago <santiago7nubesoft@gmail.com>
2021-03-07 19:24:29 -0600

    Feat: multiplexacion salida a display

--- reloj.c
+++ reloj.c
@@ -17,14 +17,13 @@ void main(){
    
    setup_adc_ports(NO_ANALOGS);
    setup_adc(ADC_OFF);
-   set_tris_a(0b000000);  //(0)salida segundo, (1)setHora, , (2)setMin
-   set_tris_c(0b00000000); //salida de bits, hora
-   set_tris_d(0b00000000); //salida de bits, minuto   
-   set_tris_e(0b111); //(0)config más de 1 seg. (1) incremento (2) decremento
+   set_tris_a(0b000000);  //(1)setHora,(2)setMin,(3)setDia,(4)setMes,(5)setanio,(6)setDiaSemana
+   set_tris_c(0b00000000); //salida multiplexor, indicar que display encender
+   set_tris_d(0b00000000); //salida de bits, multiplexación 
+   set_tris_e(0b111); //(0)config más de 1 seg. (1) decremento (2)incremento
    output_a(0x00);
    output_d(0x00);
-   output_c(0x00); 
-   
+      
    rtc_init();
       
    for(;;){   
@@ -35,9 +34,10 @@ void main(){
          if(segAnterior != segActual){    
             segAnterior = segActual;
             mostrarSegundo();
-            mostrarHoraMinuto(hora, minuto);           
+                     
          }
-         delay_ms(1000);
+          mostrarHoraMinuto(hora, minuto); 
+         
       }
       config();
    }
@@ -49,6 +49,7 @@ void config(){
    int cambio;
    int anio, mes, dia, semana, hora, minuto, segundo;   
    int inConfig, inIncremento, inDecremento;
+   delay_ms(400);
    
    while(set<6){
       inConfig = INPUT(PIN_E0);      
@@ -86,7 +87,7 @@ void config(){
          if(inIncremento == 1) cambio = 1;
          else cambio = -1;
       
-         delay_ms(600);
+         delay_ms(500);
          rtc_get_time(hora, minuto, segundo); 
          
          if(set == 0){                         // minuto
@@ -123,20 +124,42 @@ void config(){
 }
 
 void mostrarHoraMinuto(hora, minuto){
-  byte binMinuto, binHora;
-      
-  binMinuto = get_bcd(minuto);
-  binHora   = get_bcd(hora);
-         
-  output_c(binHora);
-  output_d(binMinuto);         
+  byte binMinuto, binHora, binDecMinuto, binDecHora;
+  int minutoDec, horaDec, minutoUnidad, horaUnidad;
+  
+  minutoDec = minuto / 10;
+  minutoUnidad = minuto%10;
+  horaDec   = hora / 10;
+  horaUnidad = minuto%10;
+  
+  binMinuto    = get_bcd(minutoUnidad);
+  binDecMinuto = get_bcd(minutoDec);  
+  binHora      = get_bcd(horaUnidad);
+  binDecHora   = get_bcd(horaDec);
+  
+  
+  output_d(binMinuto);
+  output_c(0b000001);
+  delay_ms(5);  
+  
+  output_d(binDecMinuto);  //decimales minuto
+  output_c(0b000010);
+  delay_ms(5);
+  
+  output_d(binhora);
+  output_c(0b000100);
+  delay_ms(5);  
+  
+  output_d(binDecHora);  //decimales minuto
+  output_c(0b001000);
+  delay_ms(5);
 }
 
 void mostrarSegundo(){
    if(OUT_SEGUNDO){
-      output_high(PIN_B7);
+      output_high(PIN_B0);
    }else{      
-      output_low(PIN_B7);
+      output_low(PIN_B0);
    }
    OUT_SEGUNDO = !OUT_SEGUNDO;
 }
